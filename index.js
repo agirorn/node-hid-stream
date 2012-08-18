b7f79221d205f900cd583e19930541b589106e24
Emily Rose <emily@contactvibe.com>
2012-08-18 04:49:36 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+;(function() {
+	
+	var 
+		hid = require('node-hid')
+		, stream = require('stream')
+		, util = require('util')
+		, hidParsers = require('./lib/parser')
+	;
+
+	function hidDevice(path, opts) {
+
+		if(!path) { return new Error("no HID path specified"); }
+		if(!opts) { opts = {}; }
+		if(!opts.parser) { opts.parser = hidParsers.raw; }
+		else if(typeof opts.parser != "function") {
+
+			return new Error("Invalid parser function specified");
+		}
+
+		this.opts = opts;
+		this.path = path;
+		this.device = new hid.HID(path);
+
+		stream.Stream.call(this);
+
+		this.parser = opts.parser.bind(this);
+		this.device.read(this.parser);
+		return this;
+	};
+
+	util.inherits(hidDevice, stream.Stream);
+
+	exports.device = hidDevice;
+	exports.parser = hidParsers;
+
+})();
