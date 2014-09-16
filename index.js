c096c0adab6418e7dd20bf0e86219b914eedf9ef
Emily Rose <emily@contactvibe.com>
2014-09-16 03:52:46 -0700

    Fix buffer, remove IIFE

--- index.js
+++ index.js
@@ -1,42 +1,38 @@
-;(function() {
-	
-	var 
-		hid = require('node-hid')
-		, stream = require('stream')
-		, util = require('util')
-		, hidParsers = require('./lib/parser')
-	;
+var 
+	hid = require('node-hid')
+	, hidParsers = require('./lib/parser')
+	, stream = require('stream')
+	, util = require('util')
+;
 
-	function hidDevice(path, opts) {
+function hidDevice(path, opts) {
 
-		if(!path) { return new Error("no HID path specified"); }
-		if(!opts) { opts = {}; }
-		if(!opts.parser) { opts.parser = hidParsers.raw; }
-		else if(typeof opts.parser != "function") {
+	if(!path) { return new Error("no HID path specified"); }
+	if(!opts) { opts = {}; }
+	if(!opts.parser) { opts.parser = hidParsers.raw; }
+	else if(typeof opts.parser != "function") {
 
-			return new Error("Invalid parser function specified");
-		}
+		return new Error("Invalid parser function specified");
+	}
 
-		this.opts = opts;
-		this.path = path;
-		this.device = new hid.HID(path);
+	this.opts = opts;
+	this.path = path;
+	this.device = new hid.HID(path);
 
-		stream.Stream.call(this);
+	stream.Stream.call(this);
 
-		this.parser = opts.parser.bind(this);
-		this.device.read(this.parser);
-		return this;
-	};
+	this.parser = opts.parser.bind(this);
+	this.device.read(this.parser);
+	return this;
+};
 
-	function getDevices() {
+function getDevices() {
 
-		return hid.devices();
-	};
+	return hid.devices();
+};
 
-	util.inherits(hidDevice, stream.Stream);
+util.inherits(hidDevice, stream.Stream);
 
-	exports.device = hidDevice;
-	exports.parser = hidParsers;
-	exports.getDevices = getDevices;
-
-})();
+exports.device = hidDevice;
+exports.parser = hidParsers;
+exports.getDevices = getDevices;
